88a91bbc3f17f05736b7c08f48bca92b72bf01c3
asrim10 <asrimmsuwall@gmail.com>
2025-02-24 08:51:50 +0545

    setup uploads

--- innertune-backend/index.js
+++ innertune-backend/index.js
@@ -4,6 +4,9 @@ import 'dotenv/config';
 import songRouter from './src/routes/songRoutes.js';
 import albumRouter from './src/routes/albumRoutes.js';
 import { Connection } from './src/database/db.js';
+import { createUploadsFolder } from './security/helper.js'; 
+import uploadRoutes from './src/routes/uploadRoutes.js';
+
 
 // app config
 const app = express();
@@ -14,12 +17,13 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
+app.use("/uploads", express.static("uploads")); // Serve static files from the "uploads" directory
+createUploadsFolder();
 
 // initializing routes
-
 app.use("/api/song", songRouter)
 app.use("/api/album", albumRouter)
-
+app.use("/api/file", uploadRoutes)
 app.get('/',(req,res)=>{
     res.send('API working');
 })
