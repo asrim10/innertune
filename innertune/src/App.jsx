0f0dc9e15f9b714cceb2d20f29258c47606a8a01
asrim10 <asrimmsuwall@gmail.com>
2025-03-18 09:28:38 +0545

    ikport

--- innertune/src/App.jsx
+++ innertune/src/App.jsx
@@ -14,7 +14,7 @@ import DisplayPlaylist from "./components/Private/DisplayPlaylists";
 import DisplayAlbum from "./components/Private/DisplayAlbum";
 import AdminDashboard from "./components/Private/AdminDashboard";
 import ProtectedRoute from "./ProtectedRoute";
-import AddSongToPlaylist from "./components/Private/AddSongToPLaylist";
+import AddSongToPlaylist from "./components/Private/AddSongToPlaylist";
 
 // Artist Pages
 import AddSong from "./pages/AddSong";
